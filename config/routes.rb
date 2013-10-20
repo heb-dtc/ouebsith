5b72b841518328dcd993acd581e3dcfe265bc529
¨flo¨ <florent.noelfl@gmail.com>
2013-10-20 16:22:44 +0200

    ´finish_static_pages´

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Ouebsith::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/about"
+  get "static_pages/photo"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
